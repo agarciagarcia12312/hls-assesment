acd1b8b5c9927a9474245d0d21b30b883796a261
AGcodes <andygarciacodes@gmail.com>
2018-03-05 18:05:51 -0700

    made changes to hls-fetch file
    added hls-fetch module and modify code inside

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 
 ======================= Hls assesment ==========
 
-description: express powerd app capable of sending locally stored videos to client
+description: express powered app capable of dowloading hls videos and sending locally stored videos to client
 
 usage: API endpoint accessible trough enpoint  "/stream?hlsurl=<hlsurl>"
+
+fetchHLS(externalUri,desiredName)
 
 video1: Playposit intro video length = 1:29
 video2: cute animal video length = 21 seconds
 defaultVideo: cute animal video length = 6 seconds
+
+Modified hls-fetch index.js file to fit requirements for project;
+added the ability to put in desired file names and a counter to correctly name all the different quality video versions
--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const app = express();
+const fls =require('hls-fetcher')
 
-// used to serve the staic video files
 app.use(express.static(path.join(__dirname,'videoFiles')));
 
+const fetchHls = function(uri,name){
+  let options={
+    uri:uri,
+    cwd:'videoFiles',
+    videoName:name
+  }
+  fls.getIt(options)
+}
+
+fetchHls(`https://player.vimeo.com/external/249414131.m3u8?s=10bf9d088fff85588fdd56dacd5f9f716c1c8dd5`,'video2');
+fetchHls(`https://player.vimeo.com/external/254060783.m3u8?s=268bba3c32a74512cf2491891689a9760bbbecfa`,'video1');
 
 
 app.get('/stream',(req,res)=>{
@@ -15,6 +26,7 @@ app.get('/stream',(req,res)=>{
   fs.exists(filePath,function (exist){
     if (!exist){
       // if file not send play default video
+      console.log('video not found')
       res.writeHead(202,{
         'Content-Type':
         'application/vnd.apple.mpegurl'
@@ -23,6 +35,7 @@ app.get('/stream',(req,res)=>{
       file.pipe(res);
 
     } else {
+      console.log('videoFound',filePath)
       // if file found send the appropoate .m3u8 file
       res.writeHead(200,{
         'Content-Type':
